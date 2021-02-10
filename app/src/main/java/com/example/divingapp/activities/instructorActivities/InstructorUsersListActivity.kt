3408220db20aed7af100c962bfad7f14127a27b7
rafalbober <rafalbober.poczta@gmail.com>
2021-02-11 00:47:07 +0100

    example RecyclerView

--- app/src/main/java/com/example/divingapp/activities/instructorActivities/InstructorUsersListActivity.kt
+++ app/src/main/java/com/example/divingapp/activities/instructorActivities/InstructorUsersListActivity.kt
@@ -2,11 +2,39 @@ package com.example.divingapp.activities.instructorActivities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.divingapp.R
+import com.example.divingapp.Utils.RecyclerAdapter
 
 class InstructorUsersListActivity : AppCompatActivity() {
+
+    private var titlesList = mutableListOf<String>()
+    private var descList = mutableListOf<String>()
+    private var imagesList = mutableListOf<Int>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_instructor_users_list)
+
+        postToList()
+
+        val rvRecyclerView : RecyclerView = findViewById(R.id.rv_recyclerView)
+        rvRecyclerView.layoutManager = LinearLayoutManager(this)
+        rvRecyclerView.adapter = RecyclerAdapter(titlesList, descList, imagesList)
     }
+
+    private fun addToList(title: String, description: String, image: Int) {
+        titlesList.add(title)
+        descList.add(description)
+        imagesList.add(image)
+    }
+
+    private fun postToList() {
+        for (i in 1..20) {
+            addToList("Title $i", "Description $i", R.mipmap.ic_launcher_round)
+        }
+    }
+
+
 }
